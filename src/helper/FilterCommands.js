8b7745739566974beee26edf801166d310e622df
Lourens <lourensros@gmail.com>
2021-05-17 19:53:40 +0200

    Add FilterCommands.js

---
+++ src/helper/FilterCommands.js
@@ -0,0 +1,24 @@
+module.exports = {
+	/**
+	 *
+	 * @param commands
+	 * @param { Message } message
+	 * @returns {Promise<*>}
+	 */
+	byPermission: async (commands, message) => {
+		// Filter all commands based on the permissions specified and the user's permissions
+		return commands.filter((command) => {
+			let viable = false;
+			if (command["permissions"].length > 0) {
+				command["permissions"].forEach((permission) => {
+					if (message.member.hasPermission(permission)) {
+						viable = true;
+					}
+				});
+			} else {
+				viable = true;
+			}
+			return viable;
+		});
+	}
+};
