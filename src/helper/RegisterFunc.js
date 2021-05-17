d94677059301a045d5afb3816729dac2269f238e
Lourens <lourensros@gmail.com>
2021-05-17 19:57:28 +0200

    Add FilterCommands to client object

--- src/helper/RegisterFunc.js
+++ src/helper/RegisterFunc.js
@@ -1,11 +1,14 @@
 // NPM Modules
 const { MessageEmbed } = require("discord.js-light");
 
+const FilterCommands = require("../helper/FilterCommands");
+
 module.exports = (client) => {
 	client.djsCommandControl =
 		{
 			getEmbed: () => (new MessageEmbed)
 				.setColor("#ffa800")
-				.setFooter(client.user.username, client.user.avatarURL())
+				.setFooter(client.user.username, client.user.avatarURL()),
+			FilterCommands,
 		};
 };
