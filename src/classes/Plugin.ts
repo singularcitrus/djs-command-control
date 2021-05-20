2202e53e303e4b6bf077ca26c066e4b16bbe9a3b
Lourens <lourensros@gmail.com>
2021-05-20 15:41:26 +0200

    Add Plugin Class

---
+++ src/classes/Plugin.ts
@@ -0,0 +1,52 @@
+import { CommandObj, DjsCommandControlClient } from "../types";
+import Command from "./Command";
+
+export default class {
+  readonly name: string;
+  private _commands: CommandObj[] = [];
+  private _initialize: (
+    djsCommandControl: DjsCommandControlClient | null
+  ) => DjsCommandControlClient | false = (arg0 = null) => false;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  /**
+   * The function to call when initializing the plugin
+   *
+   * @param value
+   */
+  set initialize(
+    value: (
+      djsCommandControl: DjsCommandControlClient | null
+    ) => DjsCommandControlClient | false
+  ) {
+    this._initialize = value;
+  }
+  get initialize(): (
+    djsCommandControl: DjsCommandControlClient | null
+  ) => DjsCommandControlClient | false {
+    return this._initialize;
+  }
+
+  /**
+   * Get bundled commands from the plugin
+   */
+  get commands(): CommandObj[] {
+    return this._commands;
+  }
+
+  /**
+   * Adds a command to bundle with the plugin
+   *
+   * @param {Command} command The command to bundle
+   */
+  addBundledCommand(command: Command) {
+    if (command instanceof Command) {
+      this._commands.push(command.command);
+    } else {
+      throw new Error("TypeError: command not an instance of Command");
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import CommandClass from "./classes/Command";
 import CommandsClass from "./classes/Commands";
+import PluginClass from "./classes/Plugin";
 
 export const Commands = CommandsClass;
 export const Command = CommandClass;
+export const Plugin = PluginClass;
 
 export * from "./types";
