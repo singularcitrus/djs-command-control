2fd6573996419f09983364f74857b259f0b38642
Lourens <lourensros@gmail.com>
2021-05-20 14:36:58 +0200

    Expose Command class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import CommandClass from "./classes/Command";
 import CommandsClass from "./classes/Commands";
 
 export const Commands = CommandsClass;
+export const Command = CommandClass;
 
 export * from "./types";
